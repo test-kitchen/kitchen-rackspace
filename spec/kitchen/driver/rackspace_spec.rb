7ea3f14d54684bc4d87a4d3ef72e25efba2fc649
Martin Smith <martin@mbs3.org>
2015-10-05 15:39:46 -0500

    Fix additional test with expected default values

--- spec/kitchen/driver/rackspace_spec.rb
+++ spec/kitchen/driver/rackspace_spec.rb
@@ -433,6 +433,7 @@ describe Kitchen::Driver::Rackspace do
         image_id: 'there',
         flavor_id: 'captain',
         public_key_path: 'tarpals',
+        no_passwd_lock: false,
         networks: default_networks
       }
     end
